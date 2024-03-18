133eb96fe0c15ef0469e1f1b28cb9583b4a3f3d2
Yuanhao Li <erwinli@qq.com>
2024-03-18 15:03:08 +0000

    Add setup.py file for project configuration

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+# Read the README.md file
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='local_picbed_mac',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        "rumps",
+        "flask",
+        "APPKit",
+    ],
+    author='ErwinLiYH',
+    author_email='erwinli@qq.com',
+    description='A minimalist local picbed for Mac OS',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS",
+    ],
+)
